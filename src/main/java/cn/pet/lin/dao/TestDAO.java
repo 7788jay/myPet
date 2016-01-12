2ddc555dee83c94132b5dd3543dc04dfbb44eeab
linweitang <linweitang@qtone.cn>
2016-01-12 18:18:48 +0800

    修复BUG

---
+++ src/main/java/cn/pet/lin/dao/TestDAO.java
@@ -0,0 +1,7 @@
+package cn.pet.lin.dao;
+
+/**
+ * Created by lin on 2016/1/12.
+ */
+public class TestDAO {
+}
---
+++ src/main/java/cn/pet/lin/domain/Test.java
@@ -0,0 +1,7 @@
+package cn.pet.lin.domain;
+
+/**
+ * Created by lin on 2016/1/12.
+ */
+public class Test {
+}
